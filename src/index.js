b378b644c019ebb98857d37f2a7f747b7cfb041e
Arindam Bose <arindam.bose@mapbox.com>
2019-10-09 20:03:13 -0700

    slick js slider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import '../styles/main.css';
 import mapboxgl from 'mapbox-gl';
 import {getPitstops, sortPitstops} from './pitstops';
+import $ from 'jquery';
+import 'slick-carousel';
 
 const startLocation = [-122.4194, 37.7749];
 
+function formatDistance(distance) {
+    const oneMile = 5280; //feet
+    const feet = (distance*oneMile)|0;//integer
+    if(feet > 1000) {
+        const rounded = Math.floor(distance*10)/10;
+        return `${rounded} mi`;
+    }else{
+        return `${feet} ft`;
+    }
+}
+
+function createPitstopDOM(pitstop) {
+    return `
+    <div class='slider-entry'>
+      <div class='slider-entry-content'>
+        <h3>${pitstop.Name}</h3>
+        <h5>${pitstop.Address}</h5>
+        <h6>${formatDistance(pitstop.distance)}</h6>
+        <h6>${pitstop.Hours}</h6>
+      </div>
+    </div>
+    `;
+}
+
+
+function updateCarousel(pitstops) {
+    $('.slider').slick('removeSlide', null, null, true);
+
+    for(const pitstop of pitstops){
+        $('.slider').slick('slickAdd', createPitstopDOM(pitstop.properties));
+    };
+}
 
 //Initialize the map
 let map = new mapboxgl.Map({
@@ -31,7 +65,14 @@ map.on('load', function (e) {
     geolocateControl.on('geolocate', function(position) {
         currentUserLocation = [position.coords.longitude, position.coords.latitude];
         pitstops = sortPitstops(pitstops, currentUserLocation);
+        updateCarousel(pitstops);
     });
+
+    $('.slider').slick({
+        dots: false
+    });
+    updateCarousel(pitstops);
+
 });
 
 
